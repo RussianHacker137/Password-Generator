ce98c324d24b4f8824605b28075f7419edc0dcae
RussianHacker137 <110288455+RussianHacker137@users.noreply.github.com>
2022-08-29 13:46:04 +0300

    Update and rename password_generator.py to main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import secrets
 import string
 
 x = None
+data = string.ascii_letters + string.digits
+
 try:
     x = int(input("Enter the length of the desired password/token: "))
 except:
     print("Incorrect input")
 if x:
-    data = string.ascii_letters + string.digits
     password = ''.join(secrets.choice(data) for i in range(x))
     print(password)
